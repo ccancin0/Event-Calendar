6ebc1445546a670bf0316cdf187e088b2385f040
Carlos <ccancino@memphis.edu>
2017-02-12 18:41:22 -0600

    Skeleton

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+	
+	# Set the default page for the web app.
+  	root to: 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
