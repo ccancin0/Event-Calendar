153622e965ec3fb761066de011a90351df97d15f
Heather Duke <hnduke@memphis.edu>
2017-04-17 00:13:54 +0000

    I updated the search function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,15 @@
 class WelcomeController < ApplicationController
 
   def index
-    @events = Event.where("title LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    @events = Event.all
+    if params[:search] != nil && params[:search].size > 0
+      @events = Event.none
+      words = params[:search].split(' ')
+      words.each do |word|
+        @events += Event.where("title LIKE ? OR description LIKE ?", "%#{word}%", "%#{word}%")
+      end
+    end
+    # @events = Event.where("title LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
   end
 
 
