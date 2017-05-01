b486e2cf15b8ffc1b48f461fddc4dc977a6b5b28
SaiM15 <vmmnknda@memphis.edu>
2017-05-01 20:31:02 +0000

    added show buttons and rvsp button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -86,6 +86,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :description, :date_range, :start, :end, :address, :latitude, :longitude, :color)
+      params.require(:event).permit(:title, :description, :date_range, :start, :end, :address, :latitude, :longitude, :category, :color)
     end
 end
